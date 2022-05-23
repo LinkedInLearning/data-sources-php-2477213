614af9db33b4730456c876701929f61a2d572878
Joe Casabona <jcasabona@gmail.com>
2022-05-23 17:01:26 -0400

    03-06 Start State

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+	function pretty_print( $output ) {
+		echo '<pre>';
+		print_r( $output );
+		echo '</pre>';
+	}
+
+	/** Returned an indexed array of all the tracks from the database */
+	function get_tracks( $mysqli ) {
+		$tracks = $mysqli->query( 'SELECT GROUP_CONCAT( DISTINCT track) FROM speakers AS track' );
+		return explode( ',', $tracks->fetch_all()[0][0] );	
+	}
+
+	/*** REPLACE WITH YOUR CONNECTION INFORMATION ***/
+	$db_host = 'localhost';
+	$db_user = 'root';
+	$db_password = 'root';
+	$db_db = 'conference';
+
+	$mysqli = new mysqli(
+		$db_host,
+		$db_user,
+		$db_password,
+		$db_db
+	);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Challenge: Submitting to a MySQL Database | Using Data Sources in PHP</title>
+        <meta name="author" value="Joe Casabona" />
+    </head>
+    <body>
+		<main>
+		
+		</main>
+		<style>
+			body {
+				background: #edf2f8;
+				font-family: 'Inter', sans-serf;
+				font-size: 18px;
+			}
+
+			main {
+				background: #FFFFFF;
+				padding: 40px;
+				margin: 0 auto;
+				width: 800px;
+			}
+
+			form div {
+				margin: 10px;
+				display: grid;
+    			grid-template-columns: 1fr 2fr;
+    			grid-gap: 20px;
+    			grid-template-areas: "label input input";
+			}
+
+			label {
+				grid-area: label;
+			}
+
+			input {
+				grid-area: input;
+				padding: 5px;
+				font-size: 18px;
+			}
+
+			textarea {
+				height: 200px;
+			}
+		</style>
+    </body>
+</html>
