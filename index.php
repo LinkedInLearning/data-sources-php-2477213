7ebf93c088033e3be7692d8bddc6fbe41c1ef097
Joe Casabona <jcasabona@gmail.com>
2022-05-23 13:24:56 -0400

    02_01 Beginning State

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Reading from Files | Using Data Sources in PHP</title>
+        <meta name="author" value="Joe Casabona" />
+    </head>
+    <body>
+		<main>
+			
+		</main>
+
+
+		<style>
+			body {
+				background: #edf2f8;
+				font-family: 'Inter', sans-serf;
+				font-size: 18px;
+			}
+
+			main {
+				background: #FFFFFF;
+				padding: 40px;
+				margin: 0 auto;
+				width: 800px;
+			}
+    </body>
+</html>
