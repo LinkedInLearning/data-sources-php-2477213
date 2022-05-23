adc157ece7849b133c6af85ab6114bcaf57060d8
Joe Casabona <jcasabona@gmail.com>
2022-05-23 14:21:04 -0400

    02-03 End state

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     print_r( $output );
     echo '</pre>';
   }
+
+  $api_key = 'f5d36126-807a-4f25-88af-bf5b029fe1b4'; // CHANGE TO YOUR API KEY
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,7 +15,17 @@
     </head>
     <body>
 		<main>
-			
+			<h1>Reading from Protected API</h1>
+			<?php
+				$callback = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/';
+				$word = 'test';
+				$url = $callback . $word . '?key=' . $api_key;
+				$lookup = json_decode( file_get_contents( $url ) );
+			?>
+			<dl>
+				<dt><?php echo $lookup[0]->meta->id; ?>(<?php echo $lookup[0]->fl; ?>)</dt>
+				<dd><?php echo $lookup[0]->shortdef[0]; ?></dd>
+			</dl>
 		</main>
 		<style>
 			body {
