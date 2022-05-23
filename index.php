65173a807a7d7326d3e5791bc97ba63b1af8606e
Joe Casabona <jcasabona@gmail.com>
2022-05-23 17:15:25 -0400

    03-06 Solution

--- index.php
+++ index.php
@@ -5,6 +5,21 @@
 		echo '</pre>';
 	}
 
+	function add_session( $mysqli, $info ) {
+		extract( $info );
+		$bio = htmlentities( $bio, ENT_QUOTES );
+		$session_desc = htmlentities( $session_desc, ENT_QUOTES );
+		$track = ( is_null( $track ) ) ? 'Tech' : $track;
+
+		$query = "INSERT INTO speakers VALUES(NULL, '$name', '$title', '$org', '$bio', '$session_name', '$session_desc', '$track');";
+
+		if( $mysqli->query( $query )  === TRUE ) {
+			echo '<p>New record added successfully!</p>';
+		} else {
+			echo '<p>Error: '. $mysqli->error . '</p>';
+		}
+	}
+
 	/** Returned an indexed array of all the tracks from the database */
 	function get_tracks( $mysqli ) {
 		$tracks = $mysqli->query( 'SELECT GROUP_CONCAT( DISTINCT track) FROM speakers AS track' );
@@ -32,7 +47,53 @@
     </head>
     <body>
 		<main>
-		
+		<h1>Add Record to Database</h1>
+			<?php
+				if( isset( $_POST['submit'] ) ) {
+					add_session( $mysqli, $_POST ); 
+				}
+			?>
+			<form name="new-speaker" method="POST">
+				<div>
+					<label for="name">Name:</label>
+					<input type="text" name="name" />
+				</div>
+				<div>
+					<label for="title">Title:</label>
+					<input type="text" name="title" />
+				</div>
+				<div>
+					<label for="org">Organization:</label>
+					<input type="text" name="org" />
+				</div>
+				<div>
+					<label for="bio">Bio:</label>
+					<textarea name="bio"></textarea>
+				</div>
+				<div>
+					<label for="session_name">Session Name:</label>
+					<input type="text" name="session_name" />
+				</div>
+				<div>
+					<label for="session_desc">Description:</label>
+					<textarea name="session_desc"></textarea>
+				</div>
+				<div>
+					<label for="track">Track:</label>
+					<select name="track">
+						<option value="null">Select a Track</option>
+						<?php 
+							$tracks = get_tracks( $mysqli );
+							foreach( $tracks as $track ) {
+								echo '<option value="'. $track .'">' . $track .'</option>';
+							}
+						?>
+					</select>
+				</div>
+				<div class="submit">
+						<input type="submit" name="submit" value="Submit Your Session!" />
+				</div>
+			</form>
 		</main>
 		<style>
 			body {
