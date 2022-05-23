8610757fcedbd556e66459f4dd92a46978760276
Joe Casabona <jcasabona@gmail.com>
2022-05-23 13:56:05 -0400

    02-02 End state

--- index.php
+++ index.php
@@ -13,7 +13,17 @@
     </head>
     <body>
 		<main>
-			
+			<h1>Reading from Open API</h1>
+			<?php 
+				$xkcd = json_decode( file_get_contents( 'https://xkcd.com/info.0.json ' ) );
+				$format = '<div><img src="%1$s" title="%2$s" alt="%3$s" /></div>';
+
+				printf( $format,
+					$xkcd->img,
+					$xkcd->title,
+					$xkcd->alt 
+				);
+			?>
 		</main>
 		<style>
 			body {
